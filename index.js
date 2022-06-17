04548ccf0c857542f65fc809ce2106b7292b579e
c04nh <w2038@e-mirim.hs.kr>
2022-06-17 15:11:09 +0900

    수정~!

--- index.js
+++ index.js
@@ -38,10 +38,23 @@ app.get("/main", function(req,res){
     db.query(sql, function (err, result, fields) {
         if (err) throw err;
         res.render('main', {result:result});
-    });
-    
-     
+    }); 
 })
+
+app.get("/addProject", function(req,res){ 
+    res.render('addProject');
+})
+
+app.get("/main/notice", function(req,res){ 
+    const sql = "select * from notice join project where notice.proid = project.proid order by notDate desc;";
+    db.query(sql, (err, row)=>{
+      if(err) {
+        console.error(err.message);
+      }
+      res.render('mainNotice', {project:row});
+    });
+})
+
 
 app.get("/main/:id/calender", function(req,res){ 
     const id = req.params.id;
@@ -108,6 +121,22 @@ app.get("/main/:id/notice", function(req,res){
     });
 })
 
+app.get("/main/:id/notice/addNotice", function(req,res){ 
+    const id = req.params.id;
+
+    res.render('addNotice');
+})
+
+app.get("/main/:id/notice/addNotice/addNot", function(req,res){
+    var id = req.params.id;
+    var content = req.query.content;
+    var name = req.query.name;
+
+    db.query('insert into notice (content, proId, userId, notDate) VALUES("' + content + '", "' + id + '", "' + name + '", DATE_FORMAT(now(), \'%Y-%m-%d\'));');
+
+    res.send("<script>window.location.replace('/main/" + id +"/notice');</script>");
+})
+
 app.get("/register", function(req,res){ 
     res.render('register'); 
 })
@@ -123,7 +152,11 @@ app.get("/send", function(req,res){
 
 })
 
-app.get("/login", function(req,res){
+app.get("/login", function(req, res){
+    res.render('login');
+});
+
+app.get("/trylogin", function(req,res){
     var id = req.query.id;
     var password = req.query.password;
 
@@ -137,7 +170,7 @@ app.get("/login", function(req,res){
             var name = result[0].userName;
             res.send("<script> window.location.replace('/main');</script>");
         }else{
-            res.send("<script>alert('아이디 또는 비밀번호를 잘못 입력하였습니다.'); window.location.replace('/login.html');</script>");
+            res.send("<script>alert('아이디 또는 비밀번호를 잘못 입력하였습니다.'); window.location.replace('/login');</script>");
         }
       });
     
@@ -152,15 +185,22 @@ app.get("/create", function(req,res){
     var id3 = req.query.id3;
     var id4 = req.query.id4;
 
-    let today = new Date();   
-    let hours = today.getHours(); // 시
-    let minutes = today.getMinutes();  // 분
-    let seconds = today.getSeconds();  // 초
-    let milliseconds = today.getMilliseconds(); // 밀리초
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+    var dateString = year + month + day;
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+    var seconds = ('0' + today.getSeconds()).slice(-2); 
+    var timeString = hours + minutes + seconds;
 
-    var proId = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
+    var len = title.length;
+    if(len > 6) len = 6;
 
-    db.query('insert into project (proId, proName, col, manager, userId1, userId2, userId3, userId4) VALUES("' + proId + '", "' + title + '", "' + color + '", "' + manager + '", "' + id1 + '", "' + id2 + '", "' + id3 + '", "' + id4 + '");');
+    var proid = dateString + timeString + title.substring(0, len);
+
+    db.query('insert into project (proId, proName, col, manager, userId1, userId2, userId3, userId4) VALUES("' + proid + '", "' + title + '", "' + color + '", "' + manager + '", "' + id1 + '", "' + id2 + '", "' + id3 + '", "' + id4 + '");');
 
     res.send("<script> window.location.replace('/main');</script>");
 
